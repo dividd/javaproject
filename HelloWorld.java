bf36d514897b4248669a77fafc4577fc55bbc428
dividd <161325586+dividd@users.noreply.github.com>
2024-03-14 10:43:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
 //The main method begins here.
 public static void main(String []args) {
-System.out.println("This is Devops");
-System.out.println("This is Jenkins Demo");
-System.out.println("And this is java program");/
+System.out.println("This is Devops1");
+System.out.println("This is Jenkins Demo1");
+System.out.println("And this is java program1");/
 / prints Hello World
 }//main method ends here
 }
